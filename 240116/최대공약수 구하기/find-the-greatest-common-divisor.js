7d14eb869eefb2783cc86aee948eb871272625e0
SUNGJAEKIM <155716683+jake0218@users.noreply.github.com>
2024-01-16 11:14:02 +0900

    Title: 최대공약수 구하기, Time: 164ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240116/최대공약수 구하기/find-the-greatest-common-divisor.js
+++ 240116/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -1,7 +1,7 @@
 let getGCD = (num1, num2) => {
     let gcd = 1;
 
-    for(let i = 2; i <= Math.min(num1,num2); i++) {
+    for(let i = 1; i <= Math.min(num1,num2); i++) {
         if(num1 % i === 0 && num2 % i === 0){
             gcd = i;
         }

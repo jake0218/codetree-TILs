d5f62bb60d650a99ba7bcb24ae88da0b157df786
SUNGJAEKIM <155716683+jake0218@users.noreply.github.com>
2024-01-17 11:04:10 +0900

    Title: 최대공약수 구하기, Time: 195ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240117/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let [n, m] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+function getGcd(n, m) {
+    let gcd = 0;
+
+    for(let i = 1; i <= Math.min(n,m); i++) {
+        if(n % i === 0 && m % i === 0){
+            gcd = i;
+        }
+    }
+   process.stdout.write(gcd + "\n");
+}
+
+(getGcd(n,m))

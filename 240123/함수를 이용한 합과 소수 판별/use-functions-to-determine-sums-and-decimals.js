a9e0d48da757b074f3e4710c9b9858c3cf7eca0e
SUNGJAE KIM <155716683+jake0218@users.noreply.github.com>
2024-01-23 10:47:19 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 229ms, Memory: 14MB, Status: Passed - CodeTree

--- 240123/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.js
+++ 240123/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.js
@@ -4,7 +4,7 @@ let [a, b] = input.split(' ').map(Number)
 
 const isPrime = (n) => {
     if(n <= 1) return false;
-    for(let i = 2; i * i < n; i++){
+    for(let i = 2; i * i <= n; i++){
         if(n % i === 0) return false; 
     }
     return true

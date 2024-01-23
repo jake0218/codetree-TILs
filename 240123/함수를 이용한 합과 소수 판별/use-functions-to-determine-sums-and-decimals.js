774a0b76d08ccd1f0027ecfbefb0f9f630215471
SUNGJAE KIM <155716683+jake0218@users.noreply.github.com>
2024-01-23 10:45:37 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240123/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.js
@@ -0,0 +1,32 @@
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().trim()
+let [a, b] = input.split(' ').map(Number)
+
+const isPrime = (n) => {
+    if(n <= 1) return false;
+    for(let i = 2; i * i < n; i++){
+        if(n % i === 0) return false; 
+    }
+    return true
+}
+
+const isEvenOfSum = (n) =>{
+    let sum = 0;
+    while(n > 0) {
+        sum += n % 10;
+        n = Math.floor(n / 10)
+    }
+    return sum % 2 === 0
+}
+
+const countPrimes = (a, b) => {
+    let count = 0;
+    for(let i = a; i <= b; i++){
+        if(isPrime(i) && isEvenOfSum(i)){
+            count++
+        }
+    }
+    return count
+}
+
+console.log(countPrimes(a, b))
